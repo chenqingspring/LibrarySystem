75d309d73289a1f0aeca4a8af4ce931968d60415
chenqingspring <chenqing1213@gmail.com>
2012-07-12 21:45:52 +0800

    menu select test added

---
+++ test/MenuSelectTest.java
@@ -0,0 +1,23 @@
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+/**
+ * Created by IntelliJ IDEA.
+ * User: spring
+ * Date: 12-7-12
+ * Time: ÏÂÎç8:48
+ * To change this template use File | Settings | File Templates.
+ */
+public class MenuSelectTest {
+
+    @Test
+    public void test_menu_select(){
+        Menu m = new Menu();
+        Customers customers = new Customers();
+        assertEquals(true,customers.select_menu(1));
+        assertEquals(true,customers.select_menu(2));
+        assertEquals(true,customers.select_menu(3));
+        assertEquals(false,customers.select_menu(4));
+        assertEquals(false,customers.select_menu(0));
+    }
+}
