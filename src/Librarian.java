3b52f8e8dd576c4b7d7b1690d40cbbf0f2a298e4
chenqingspring <chenqing1213@gmail.com>
2012-08-01 17:22:09 +0800

    removed the useless files

--- src/Librarian.java
+++
@@ -1,9 +0,0 @@
-/**
- * Created by IntelliJ IDEA.
- * User: spring
- * Date: 12-7-11
- * Time: обнГ4:34
- * To change this template use File | Settings | File Templates.
- */
-public class Librarian {
-}
